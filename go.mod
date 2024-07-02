1983770ebeb71d5fbbe1f287eca46f0caa22092d
chrishorton <hortonchristopher27@gmail.com>
2024-07-02 09:45:59 -0600

    some changes

--- go.mod
+++ go.mod
@@ -2,15 +2,30 @@ module github.com/chrishorton/spacerecon
 
 go 1.21
 
-require github.com/spf13/viper v1.19.0
+require (
+	github.com/manifoldco/promptui v0.9.0
+	github.com/spf13/viper v1.19.0
+	github.com/stretchr/testify v1.9.0
+)
 
 require (
+	github.com/charmbracelet/bubbletea v0.26.6 // indirect
+	github.com/charmbracelet/x/ansi v0.1.2 // indirect
+	github.com/charmbracelet/x/input v0.1.0 // indirect
+	github.com/charmbracelet/x/term v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.1.0 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 )
 
 require (
@@ -30,7 +45,7 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -14,8 +14,14 @@ func main() {
 	config.LoadConfig(".")
 	prompt := promptui.Select{
 		Label: "Select Command",
-		Items: []string{"Query TLE", "Starlink Satellites", "Exit"},
+		Items: []string{"TLE and OMM", "Starlink Satellites", "Exit"},
 	}
+
+    TLESubmenu := promptui.Select{
+        Label: "Select TLE Command",
+        Items: []string{"Query TLE", "Load TLE From File", "Load TLE From Space-Track.org", "Main Menu"},
+    }
+
 	for {
 		_, result, err := prompt.Run()
 
@@ -26,15 +32,42 @@ func main() {
 
 		switch result {
 		case "Query TLE":
-			// Handle TLE query
-			fmt.Println("Querying TLE...")
-			// Add TLE query functionality here
-            tle.Get()
+            _, result, err := TLESubmenu.Run()
+            if err != nil {
+                fmt.Printf("Prompt failed %v\n", err)
+                return
+            }
+
+            for {
+                switch result {
+                case "Query TLE":
+                    fmt.Println("Fetching TLE...")
+                    tles, err := tle.Get()
+                    if err != nil {
+                        fmt.Println("Error fetching TLE: ", err)
+                    }
+                    for _, t := range tles {
+                        fmt.Println(t)
+                    }
+                case "Load TLE From File":
+                    fmt.Println("Loading TLE from file...")
+                case "Load TLE From Space-Track.org":
+                    fmt.Println("Loading TLE from Space-Track.org...")
+                case "Exit":
+                    return
+                }
+            }
 
 		case "Starlink Satellites":
 			// Handle Starlink satellites query
 			fmt.Println("Fetching Starlink satellites...")
-			starlink.Get()
+            sats, err := starlink.Get()
+            if err != nil {
+                fmt.Println("Error fetching Starlink satellites: ", err)
+            }
+            for _, s := range sats {
+                fmt.Println(s)
+            }
 
 		case "Exit":
 			fmt.Println("Exiting...")
